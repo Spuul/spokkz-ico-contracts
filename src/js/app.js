f1a30fa3bb9f3811274d62604f805eb0ceccfcae
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-03-12 16:54:50 +0800

    Account information

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,79 @@
 App = {
-     web3Provider: null,
-     contracts: {},
+  web3Provider: null,
+  contracts: {},
+  account: 0x0,
 
-     init: function() {
-          /*
-           * Replace me...
-           */
+  init: function() {
+    return App.initWeb3();
+  },
 
-          return App.initWeb3();
-     },
+  initWeb3: function() {
+    // initialize web3
+    if (typeof web3 !== 'undefined') {
+      //reuse the provider of the Web3 object injected by Metamask
+      App.web3Provider = web3.currentProvider;
+    } else {
+      //create a new provider and plug it directly into our local node
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
-     initWeb3: function() {
-          /*
-           * Replace me...
-           */
+    App.displayAccountInfo();
 
-          return App.initContract();
-     },
+    return App.initContract();
+  },
 
-     initContract: function() {
-          /*
-           * Replace me...
-           */
-     },
+  initContract: function() {
+    /*
+     * Replace me...
+     */
+  },
+
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $('#accountAddress').text(account);
+        web3.eth.getBalance(account, function(err, balance) {
+          if (err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+          }
+        })
+      }
+    });
+
+    web3.version.getNetwork((err, netId) => {
+      var networkType;
+
+      switch (netId) {
+        case "1":
+          networkType = "Main Net";
+          break
+        case "2":
+          networkType = "Morden Test (Deprecated)";
+          break
+        case "3":
+          networkType = "Ropsten Test";
+          break
+        case "4":
+          networkType = "Rinkeby Test";
+          break
+        case "42":
+          networkType = "Kovan Test";
+          break
+        default:
+          console.log('This is an unknown network. ' + netId)
+          networkType = "Development (Ganache)";
+      }
+
+      $('#networkId').text(networkType);
+
+    });
+  },
 };
 
 $(function() {
-     $(window).load(function() {
-          App.init();
-     });
+  $(window).on('load', function() {
+    App.init();
+  });
 });
