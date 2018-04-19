7c6eb7a28ff988c3e0963924510b0f0ed50b07f5
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-04-20 01:49:09 +0800

    Clean up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,31 +3,38 @@ var SpokTokenSale = artifacts.require("./SpokTokenSale.sol")
 
 module.exports = function(deployer, network, accounts) {
 
-  // Testing Parameters
-  // ==================
+
   // INFO: Commented out the TimedCrowdsale require(_openingTime >= now) for easy testing.
-  const openingTime = Math.round((new Date(Date.now() - 86400000).getTime()) / 1000); // Yesterday
-  const closingTime = Math.round((new Date().getTime() + (86400000 * 20)) / 1000); // Today + 20 days
-
-  const rateDuringPrivateStage = 2000000; // 1 ETH will get 2 million tokens, about 50% discount
-  const rateDuringPreICOStage = 1176470; // 1 ETH will get 1176470 tokens, about 15% discount
-  const rateDuringICOStage = 1000000; // 1 ETH will get 1 million tokens, no discount
-
-  const goal = 5000000000000000000; // goal is 5 ethers
-  const cap = 300000000000000000000; // cap is 300 ethers
-  // ==================
-
-
+  // const tokenSaleDays = 45;
+  //
+  // const openingTime = Math.round((new Date(Date.now() - 86400000).getTime()) / 1000);
+  // const closingTime = Math.round((new Date().getTime() + (86400000 * tokenSaleDays)) / 1000);
+  //
+  const rateDuringPrivateStage = 12000;
+  const rateDuringPreICOStage = 7058;
+  const rateDuringICOStage = 6000;
+  //
+  // const goal = 11111000000000000000000;   // The goal is 11,111 Ethers
+  const cap = 50000000000000000000000;    // The cap is 50,000 Ethers
 
   const wallet = accounts[9];
 
-
   deployer.deploy(SpokToken).then(function() {
     return deployer.deploy(
-      SpokTokenSale, rateDuringPrivateStage, rateDuringPreICOStage, rateDuringICOStage, wallet, SpokToken.address, cap, goal, openingTime, closingTime).then(function() {
+      SpokTokenSale, rateDuringPrivateStage, rateDuringPreICOStage, rateDuringICOStage, wallet, SpokToken.address, cap).then(function() {
       return SpokToken.deployed().then(function(spokToken) {
         return spokToken.transferOwnership(SpokTokenSale.address);
       });
     });
   })
+
+
+  // deployer.deploy(SpokToken).then(function() {
+  //   return deployer.deploy(
+  //     SpokTokenSale, rateDuringPrivateStage, rateDuringPreICOStage, rateDuringICOStage, wallet, SpokToken.address, cap, goal, openingTime, closingTime).then(function() {
+  //     return SpokToken.deployed().then(function(spokToken) {
+  //       return spokToken.transferOwnership(SpokTokenSale.address);
+  //     });
+  //   });
+  // })
 };
