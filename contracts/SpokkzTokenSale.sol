6e678e7159ec9bc2e33fef471a71bda9aa69c7ec
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-04-23 18:15:34 +0800

    Check if total minted tokens is within the range.

--- contracts/SpokkzTokenSale.sol
+++ contracts/SpokkzTokenSale.sol
@@ -65,4 +65,15 @@ contract SpokkzTokenSale is CappedCrowdsale, MintedCrowdsale, WhitelistedCrowdsa
 
     }
   // =============
+
+
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+
+    if (stage == TokenSaleStage.Private) {
+      uint256 tokensThatWillBeMintedAfterPurchase = msg.value.mul(rate);
+      uint256 totalTokensThatWillBeMintedAfterPurchase = tokensThatWillBeMintedAfterPurchase + token.totalSupply();
+      require(totalTokensForSalePerStage[uint256(TokenSaleStage.Private)] >= totalTokensThatWillBeMintedAfterPurchase);
+    }
+  }
 }
