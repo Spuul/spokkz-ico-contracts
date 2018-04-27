ed34cfc8651c22f5ecf6656f85d58b2412434da9
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-04-27 11:40:04 +0800

    Test for capped crowdsale

--- test/07-Capped/SpokkzTokenSale.test.js
+++ test/07-Capped/SpokkzTokenSale.test.js
@@ -14,7 +14,7 @@ const scaleDownValue = 1000;
 const rateDuringPrivateStage = new BigNumber(12000).times(scaleDownValue);
 const rateDuringPresaleStage = new BigNumber(7058).times(scaleDownValue);
 const rateDuringCrowdsaleStage = new BigNumber(6000).times(scaleDownValue);
-const cap = ether(50000).dividedBy(scaleDownValue); // 50 ethers
+const cap = ether(45000).dividedBy(scaleDownValue);
 
 const capTokenSupply = new BigNumber('1e27');        // 1 billion tokens
 const TOTAL_TOKENS_FOR_SALE = new BigNumber('3e26'); // 300 million tokens 300 000 000
@@ -28,14 +28,14 @@ const should = require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('SpokkzTokenSale', function ([_, owner, wallet, thirdparty, ecosystemFund, unsoldTokensForDistribution, otherFunds]) {
-  describe('Finalizing crowdsale', function () {
+contract('SpokkzTokenSale', function ([_, owner, wallet, investorA, ecosystemFund, unsoldTokensForDistribution, otherFunds]) {
+  describe('Capped token sale', function () {
       before(async function () {
         // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
         await advanceBlock();
       });
 
-      beforeEach(async function () {
+      before(async function () {
         this.openingTime = latestTime() + duration.weeks(1);
         this.closingTime = this.openingTime + duration.weeks(1);
         this.afterClosingTime = this.closingTime + duration.seconds(1);
@@ -55,42 +55,44 @@ contract('SpokkzTokenSale', function ([_, owner, wallet, thirdparty, ecosystemFu
           otherFunds,
            { from: owner });
         await this.token.transferOwnership(this.crowdsale.address);
+        await increaseTimeTo(this.openingTime);
+
+        await this.crowdsale.startNextSaleStage({from: owner }); // presale
+        await this.crowdsale.startNextSaleStage({from: owner }); // crowdsale
+
+        await this.crowdsale.addToWhitelist(investorA, {from: owner});
       });
 
-      it('cannot be finalized before ending', async function () {
-        await this.crowdsale.finalize({ from: thirdparty }).should.be.rejectedWith(EVMRevert);
+      it('should be successful if below the cap', async function () {
+        await this.crowdsale.sendTransaction({ value: ether(44), from: investorA}).should.be.fulfilled;
+
+        let capReached = await this.crowdsale.capReached();
+        let weiRaised = await this.crowdsale.weiRaised.call();
+        let cap = await this.crowdsale.cap.call();
+        capReached.should.equal(false);
       });
 
-      it('cannot be finalized by third party after ending', async function () {
-        await increaseTimeTo(this.afterClosingTime);
-        await this.crowdsale.finalize({ from: thirdparty }).should.be.rejectedWith(EVMRevert);
+      it('should be successful if equal to cap', async function () {
+        await this.crowdsale.sendTransaction({ value: ether(1), from: investorA}).should.be.fulfilled;
+
+        let capReached = await this.crowdsale.capReached();
+        let weiRaised = await this.crowdsale.weiRaised.call();
+        let cap = await this.crowdsale.cap.call();
+
+        weiRaised.should.be.bignumber.equal(cap);
+        capReached.should.equal(true);
+
       });
 
-      it('can be finalized by owner after ending', async function () {
-        await increaseTimeTo(this.afterClosingTime);
-        await this.crowdsale.finalize({ from: owner }).should.be.fulfilled;
+      it('should fail if more than cap', async function () {
+        await this.crowdsale.sendTransaction({ value: ether(0.001), from: investorA}).should.rejectedWith(EVMRevert);
+
+        let capReached = await this.crowdsale.capReached();
+        let weiRaised = await this.crowdsale.weiRaised.call();
+        let cap = await this.crowdsale.cap.call();
+
+        weiRaised.should.be.bignumber.equal(cap);
+        capReached.should.equal(true);
       });
-
-      it('cannot be finalized twice', async function () {
-        await increaseTimeTo(this.afterClosingTime);
-        await this.crowdsale.finalize({ from: owner });
-        await this.crowdsale.finalize({ from: owner }).should.be.rejectedWith(EVMRevert);
-      });
-
-      it('logs finalized', async function () {
-        await increaseTimeTo(this.afterClosingTime);
-        const { logs } = await this.crowdsale.finalize({ from: owner });
-        const event = logs.find(e => e.event === 'Finalized');
-        should.exist(event);
-      });
-
-      it('should mint all tokens after finalizing', async function() {
-        await increaseTimeTo(this.afterClosingTime);
-        await this.crowdsale.finalize({ from: owner }).should.be.fulfilled;
-
-        const totalSupply = await this.token.totalSupply();
-
-        totalSupply.should.be.bignumber.equal(capTokenSupply);
-      });
-    });
+  });
 });
