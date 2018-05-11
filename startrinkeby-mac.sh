ddcfd439d8e4690eb6119ee20872a112f93ad4d0
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-05-11 19:27:50 +0800

    Add cors

--- startrinkeby-mac.sh
+++ startrinkeby-mac.sh
@@ -1,1 +1,1 @@
-geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "~/Library/Ethereum/geth.ipc"
+geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "~/Library/Ethereum/geth.ipc" --rpccorsdomain "https://remix.ethereum.org"
